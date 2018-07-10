34879f2272a2320af091bad058d6fe8e540179d2
Krystian Kozłowski <34834953+krystek97@users.noreply.github.com>
2018-07-10 10:08:26 +0200

    Comment
    v 1.0

---
+++ Commentary/src/comment/sportcode/pl/Comment.java
@@ -0,0 +1,15 @@
+package comment.sportcode.pl;
+
+public class Comment 
+{
+	public static void main(String[] args)
+	{
+		//Komentarz mogacy zmiescic sie tylko w jednej linijce kodu
+		
+		
+		/* Komentarz mogacy
+		 * zmiescic sie w kilku linijkach
+		 * kodu
+		 */
+	}
+}
